1da62b398e347a0ac40411f16a3bad02dee01df0
FDR <delrincon@gmail.com>
2022-03-31 20:04:57 -0300

    sideMenu temporal - done

---
+++ components/ui/SideMenu.tsx
@@ -0,0 +1,115 @@
+import { Box, Divider, Drawer, IconButton, Input, InputAdornment, List, ListItem, ListItemIcon, ListItemText, ListSubheader } from "@mui/material"
+import { AccountCircleOutlined, AdminPanelSettings, CategoryOutlined, ConfirmationNumberOutlined, EscalatorWarningOutlined, FemaleOutlined, LoginOutlined, MaleOutlined, SearchOutlined, VpnKeyOutlined } from "@mui/icons-material"
+
+// TODO: si por lo que sea tengo problemas, en clase 176 esta el codigo listo para sobreescribir este ;)
+
+export const SideMenu = () => {
+  return (
+    <Drawer
+        open={ true }
+        // close={}
+        anchor='right'
+        sx={{ backdropFilter: 'blur(4px)', transition: 'all 0.5s ease-out' }}
+    >
+        <Box sx={{ width: 250, paddingTop: 5 }}>
+            <List>
+
+                <ListItem>
+                    <Input
+                        type='text'
+                        placeholder="Buscar..."
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                >
+                                    <SearchOutlined />
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </ListItem>
+
+                <ListItem button>
+                    <ListItemIcon>
+                        <AccountCircleOutlined/>
+                    </ListItemIcon>
+                    <ListItemText primary={'Perfil'} />
+                </ListItem>
+
+                <ListItem button>
+                    <ListItemIcon>
+                        <ConfirmationNumberOutlined/>
+                    </ListItemIcon>
+                    <ListItemText primary={'Mis Ordenes'} />
+                </ListItem>
+
+
+
+                <ListItem button>
+                    <ListItemIcon>
+                        <VpnKeyOutlined/>
+                    </ListItemIcon>
+                    <ListItemText primary={'Ingresar'} />
+                </ListItem>
+
+                <ListItem button>
+                    <ListItemIcon>
+                        <LoginOutlined/>
+                    </ListItemIcon>
+                    <ListItemText primary={'Salir'} />
+                </ListItem>
+
+                {/* Link se muestran condicional segun ancho de pantalla */}
+                <Divider sx={{ display: { xs: '', sm: 'none' } }} />
+                <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
+                    <ListItemIcon>
+                        <MaleOutlined/>
+                    </ListItemIcon>
+                    <ListItemText primary={'Hombres'} />
+                </ListItem>
+
+                <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
+                    <ListItemIcon>
+                        <FemaleOutlined/>
+                    </ListItemIcon>
+                    <ListItemText primary={'Mujeres'} />
+                </ListItem>
+
+                <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
+                    <ListItemIcon>
+                        <EscalatorWarningOutlined/>
+                    </ListItemIcon>
+                    <ListItemText primary={'Niños'} />
+                </ListItem>
+                <Divider sx={{ display: { xs: '', sm: 'none' } }} />
+
+
+                {/* Admin */}
+                <Divider />
+                <ListSubheader>Admin Panel</ListSubheader>
+
+                <ListItem button>
+                    <ListItemIcon>
+                        <CategoryOutlined/>
+                    </ListItemIcon>
+                    <ListItemText primary={'Productos'} />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <ConfirmationNumberOutlined/>
+                    </ListItemIcon>
+                    <ListItemText primary={'Ordenes'} />
+                </ListItem>
+
+                <ListItem button>
+                    <ListItemIcon>
+                        <AdminPanelSettings/>
+                    </ListItemIcon>
+                    <ListItemText primary={'Usuarios'} />
+                </ListItem>
+            </List>
+        </Box>
+    </Drawer>
+  )
+}
