6a6c96bead97cba103dbea2581f177da46c1f843
FDR <delrincon@gmail.com>
2022-03-31 19:40:33 -0300

    homePage-Temporal data - done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next';
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 
 import { ShopLayout } from '../components/layouts';
+import { initialData } from '../database/products';
+
+
 
 
 const Home: NextPage = () => {
@@ -10,6 +13,27 @@ const Home: NextPage = () => {
             <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de Teslo aqui'} >
                 <Typography variant='h1' component='h1'>Tienda</Typography>
                 <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+
+
+
+                <Grid container spacing={4}>
+                    {
+                        initialData.products.map( product => (
+                            <Grid item xs={6} sm={4} key={product.slug}>
+                                <Card>
+                                    <CardActionArea>
+                                        <CardMedia 
+                                            component='img'
+                                            image={ `products/${ product.images[0] }` }
+                                            alt={ product.title }
+                                        />
+                                    </CardActionArea>
+                                </Card>
+                            </Grid>
+                        ))
+                    }
+                </Grid>
+
             </ShopLayout>
         </>
     )
