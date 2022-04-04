566720237eec37381d2489b42807d655cfd406e5
FDR <delrincon@gmail.com>
2022-04-04 20:50:23 -0300

    UI context all set it up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { SWRConfig } from 'swr';
 
 import { lightTheme } from '../themes';
+import { UiProvider } from '../context';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -14,10 +15,12 @@ function MyApp({ Component, pageProps }: AppProps) {
                 fetcher: ( resource, init) => fetch(resource, init).then(res => res.json())
             }}
         >
-            <ThemeProvider theme={ lightTheme }>
-                <CssBaseline />            
-                <Component {...pageProps} />
-            </ThemeProvider>
+            <UiProvider>
+                <ThemeProvider theme={ lightTheme }>
+                    <CssBaseline />            
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </UiProvider>
         </SWRConfig>
     )
 }
