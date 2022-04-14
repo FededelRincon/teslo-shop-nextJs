6d4a2d24114621eaf03d3599aab945eb4e3f2cda
FDR <delrincon@gmail.com>
2022-04-14 20:47:42 -0300

    endpoint /api/user/register and /api/user/validate-token + isValidtoken function

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -22,24 +22,24 @@ export const signToken = ( _id: string, email: string ) => {
 
 
 
-// export const isValidToken = ( token: string ):Promise<string> => {
-//     if ( !process.env.JWT_SECRET_SEED ) {
-//         throw new Error('No hay semilla de JWT - Revisar variables de entorno');
-//     }
+export const isValidToken = ( token: string ):Promise<string> => {
+    if ( !process.env.JWT_SECRET_SEED ) {
+        throw new Error('No hay semilla de JWT - Revisar variables de entorno');
+    }
 
-//     return new Promise( (resolve, reject) => {
+    return new Promise( (resolve, reject) => {
 
-//         try {
-//             jwt.verify( token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
-//                 if ( err ) return reject('JWT no es válido');
+        try {
+            jwt.verify( token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
+                if ( err ) return reject('JWT no es válido');
 
-//                 const { _id } = payload as { _id: string };
+                const { _id } = payload as { _id: string };
 
-//                 resolve(_id);
+                resolve(_id);
 
-//             })
-//         } catch (error) {
-//             reject('JWT no es válido');
-//         }
-//     })
-// }
+            })
+        } catch (error) {
+            reject('JWT no es válido');
+        }
+    })
+}
