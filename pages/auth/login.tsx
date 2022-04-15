f77abe8597503facdb65ac03e4f085f1eb864b66
FDR <delrincon@gmail.com>
2022-04-15 16:07:24 -0300

    react-hook-form added to pages/auth done

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,46 +1,94 @@
 import NextLink from 'next/link';
 import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { useForm } from 'react-hook-form';
+
 import { AuthLayout } from '../../components/layouts'
+import { validations } from '../../utils';
+
+
+type FormData = {
+    email: string,
+    password: string,
+};
 
 
 const loginPage = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    const onLoginUser = ( data: FormData) => {
+        console.log({data})
+    }
+
     return (
         <AuthLayout title='Ingresar' >
-            <Box sx={{ width: 350, padding:'10px 20px'}}>
-                <Grid container spacing={2} >
-                    <Grid item xs={12}>
-                        <Typography variant='h1' component="h1">Iniciar Sesion</Typography>
+            <form onSubmit={ handleSubmit(onLoginUser) } noValidate >
+                <Box sx={{ width: 350, padding:'10px 20px'}}>
+                    <Grid container spacing={2} >
+                        <Grid item xs={12}>
+                            <Typography variant='h1' component="h1">Iniciar Sesion</Typography>
+                        </Grid>
+
+                        {/* correo */}
+                        <Grid item xs={12}>
+                            <TextField 
+                                autoComplete='off'
+                                type="email"
+                                label="Correo" 
+                                variant="filled" 
+                                fullWidth
+                                { ...register('email', {
+                                    required: 'Este campo es requerido',
+                                    validate: validations.isEmail
+                                })}
+                                error = { !!errors.email }
+                                helperText={ errors.email?.message }
+                            />
+                        </Grid>
+
+                        {/* Contraseña */}
+                        <Grid item xs={12}>
+                            <TextField 
+                                autoComplete='off'
+                                label="Contraseña" 
+                                type="password" 
+                                variant="filled" 
+                                fullWidth 
+                                { ...register('password',{
+                                    required: 'Este campo es requerido',
+                                    minLength: { value: 6, message: 'Minimo de 6 caracteres'}
+                                })}
+                                error = { !!errors.password }
+                                helperText={ errors.password?.message }
+                            />
+                        </Grid>
+
+                        {/* Boton login */}
+                        <Grid item xs={12}>
+                            <Button 
+                                type="submit"
+                                color="secondary" 
+                                className="circular-btn" 
+                                size='large' 
+                                fullWidth
+                            >
+                                Ingresar
+                            </Button>
+                        </Grid>
+
+                        {/* no tenes cuenta? */}
+                        <Grid item xs={12} display='flex' justifyContent='end'>
+                            <NextLink href='/auth/register' passHref>
+                                <Link underline='always' >
+                                    ¿ No tienes cuenta ?
+                                </Link>
+                            </NextLink>
+                        </Grid>
+
+
                     </Grid>
-
-                    {/* correo */}
-                    <Grid item xs={12}>
-                        <TextField label="Correo" variant="filled" fullWidth />
-                    </Grid>
-
-                    {/* Contraseña */}
-                    <Grid item xs={12}>
-                        <TextField label="Contraseña" type="password" variant="filled" fullWidth />
-                    </Grid>
-
-                    {/* Boton login */}
-                    <Grid item xs={12}>
-                        <Button color="secondary" className="circular-btn" size='large' fullWidth>
-                            Ingresar
-                        </Button>
-                    </Grid>
-
-                    {/* no tenes cuenta? */}
-                    <Grid item xs={12} display='flex' justifyContent='end'>
-                        <NextLink href='/auth/register' passHref>
-                            <Link underline='always' >
-                                ¿ No tienes cuenta ?
-                            </Link>
-                        </NextLink>
-                    </Grid>
-
-
-                </Grid>
-            </Box>
+                </Box>
+            </form>
         </AuthLayout>
     )
 }
