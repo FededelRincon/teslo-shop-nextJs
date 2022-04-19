83148c470158fbfd9b001c9c21f1331850abc039
FDR <delrincon@gmail.com>
2022-04-19 11:52:00 -0300

    verificar auth from token by SSR

--- pages/checkout/address.tsx
+++ pages/checkout/address.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from 'next';
 import { Box, Button, FormControl, Grid, MenuItem, Select, TextField, Typography } from "@mui/material"
-import { ShopLayout } from "../../components/layouts"
+import { ShopLayout } from "../../components/layouts";
+import { jwt } from '../../utils';
 
 
 const addressPage = () => {
@@ -73,5 +75,37 @@ const addressPage = () => {
         </ShopLayout>
     )
 }
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+
+    const { token = '' } = req.cookies;
+    let isValidToken = false;
+
+    try {
+        await jwt.isValidToken( token );
+        isValidToken = true;
+
+    } catch (error) {
+        isValidToken = false;
+    }
+
+    if( !isValidToken ) {
+        return {
+            redirect: {
+                destination: '/auth/login?p=/checkout/address',
+                permanent: false,
+            }
+        }
+    }
+
+    return {
+        props: {
+            
+        }
+    }
+}
+
 
 export default addressPage
